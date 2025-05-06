54dfe8206b01c15ef518bfd46f5b9514c5853982
Kevin Imanuel <kevimanuel12345@gmail.com>
2025-05-06 19:05:17 +0700

    tugas kelompok abstract classes

--- tugas_kelompok_1/main.cpp
+++ tugas_kelompok_1/main.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <vector>
 #include <optional>
+#include <functional>
+
 
 using namespace std;
 
@@ -22,6 +24,13 @@ enum class SortBy {
     LON
 };
 
+typedef struct {
+    optional<SortAlgorithms> sorting_algorithms = nullopt;
+    optional<SortBy> sort_by = nullopt;
+    optional<int> num_to_display = nullopt;
+    bool descending = false;
+} SortingSettings;
+
 
 class CLIParser {
 public:
@@ -38,32 +47,7 @@ public:
                 for (auto &ch: str) {
                     ch = static_cast<char>(tolower(ch));
                 }
-                if (str == "bubble") {
-                    this->sort_algorithm.emplace(SortAlgorithms::BUBBLE);
-                    continue;
-                }
-                if (str == "insertion") {
-                    this->sort_algorithm.emplace(SortAlgorithms::INSERTION);
-                    continue;
-                }
-                if (str == "merge") {
-                    this->sort_algorithm.emplace(SortAlgorithms::MERGE);
-                    continue;
-                }
-                if (str == "quick") {
-                    this->sort_algorithm.emplace(SortAlgorithms::QUICK);
-                    continue;
-                }
-                if (str == "heap") {
-                    this->sort_algorithm.emplace(SortAlgorithms::HEAP);
-                    continue;
-                }
-                if (str == "std") {
-                    this->sort_algorithm.emplace(SortAlgorithms::STD);
-                    continue;
-                }
-
-                cout << "SORTING ALGORITHM " << str << " NOT SUPPORTED" << endl;
+                set_sort_algorithm(str);
                 continue;
             }
 
@@ -72,28 +56,7 @@ public:
                 for (auto &ch: str) {
                     ch = static_cast<char>(tolower(ch));
                 }
-                if (str == "name") {
-                    this->sort_by.emplace(SortBy::NAME);
-                    continue;
-                }
-                if (str == "country") {
-                    this->sort_by.emplace(SortBy::COUNTRY);
-                    continue;
-                }
-                if (str == "population") {
-                    this->sort_by.emplace(SortBy::POPULATION);
-                    continue;
-                }
-                if (str == "lat") {
-                    this->sort_by.emplace(SortBy::LAT);
-                    continue;
-                }
-                if (str == "lon") {
-                    this->sort_by.emplace(SortBy::LON);
-                    continue;
-                }
-                cout << "SORT BY KEY " << str << " NOT AVAILABLE" << endl;
-                continue;
+                set_sort_by(str);
             }
 
             if (strstr(argv[i], "-n")) {
@@ -116,26 +79,17 @@ public:
     }
 
     void print_settings() const {
-        cout << static_cast<int>(this->sort_algorithm.value()) << endl;
-        cout << static_cast<int>(this->sort_by.value()) << endl;
-        cout << this->num_to_display.value() << endl;
+        cout << (this->sort_algorithm.has_value() ? static_cast<int>(this->sort_algorithm.value()) : -1) << endl;
+        cout << (this->sort_by.has_value() ? static_cast<int>(this->sort_by.value()) : -1) << endl;
+        cout << this->num_to_display.value_or(-1) << endl;
         cout << this->descending << endl;
     }
 
-    [[nodiscard]] optional<SortAlgorithms> get_sort_algorithm() const {
-        return sort_algorithm;
-    }
-
-    [[nodiscard]] optional<SortBy> get_sort_by() const {
-        return sort_by;
-    }
-
-    [[nodiscard]] optional<int> get_num_to_display() const {
-        return num_to_display;
-    }
-
-    [[nodiscard]] bool is_descending() const {
-        return descending;
+    [[nodiscard]] SortingSettings get_sorting_settings() const {
+        return SortingSettings {
+            .sorting_algorithms = this->sort_algorithm, .sort_by = this->sort_by,
+            .num_to_display = this->num_to_display, .descending = this->descending
+        };
     }
 
 private:
@@ -146,8 +100,126 @@ private:
     optional<SortBy> sort_by = nullopt;
     optional<int> num_to_display = nullopt;
     bool descending = false;
+
+    void set_sort_by(const string &str) {
+        if (str == "name") {
+            this->sort_by.emplace(SortBy::NAME);
+            return;
+        }
+        if (str == "country") {
+            this->sort_by.emplace(SortBy::COUNTRY);
+            return;
+        }
+        if (str == "population") {
+            this->sort_by.emplace(SortBy::POPULATION);
+            return;
+        }
+        if (str == "lat") {
+            this->sort_by.emplace(SortBy::LAT);
+            return;
+        }
+        if (str == "lon") {
+            this->sort_by.emplace(SortBy::LON);
+            return;
+        }
+        cout << "SORT BY KEY " << str << " NOT AVAILABLE" << endl;
+    }
+
+    void set_sort_algorithm(const string &str) {
+        if (str == "bubble") {
+            this->sort_algorithm.emplace(SortAlgorithms::BUBBLE);
+            return;
+        }
+        if (str == "insertion") {
+            this->sort_algorithm.emplace(SortAlgorithms::INSERTION);
+            return;
+        }
+        if (str == "merge") {
+            this->sort_algorithm.emplace(SortAlgorithms::MERGE);
+            return;
+        }
+        if (str == "quick") {
+            this->sort_algorithm.emplace(SortAlgorithms::QUICK);
+            return;
+        }
+        if (str == "heap") {
+            this->sort_algorithm.emplace(SortAlgorithms::HEAP);
+            return;
+        }
+        if (str == "std") {
+            this->sort_algorithm.emplace(SortAlgorithms::STD);
+            return;
+        }
+        cout << "SORTING ALGORITHM " << str << " NOT SUPPORTED" << endl;
+    }
 };
 
+template<typename T>
+class Sorter {
+public:
+    virtual ~Sorter() = default;
+
+    Sorter(vector<T> data, std::function<int(T, T)> cmp_fn): data(data), cmp_fn(cmp_fn) {
+    };
+
+    virtual void sort() = 0;
+
+private:
+    vector<T> data;
+    std::function<int(T, T)> cmp_fn;
+};
+
+template<typename T>
+class BubbleSorter : public Sorter<T> {
+public:
+    void sort() override {
+    }
+};
+
+template<typename T>
+class InsertionSorter : public Sorter<T> {
+public:
+    void sort() override {
+    }
+};
+
+template<typename T>
+class MergeSorter : public Sorter<T> {
+public:
+    void sort() override {
+    }
+};
+
+template<typename T>
+class QuickSorter : public Sorter<T> {
+public:
+    void sort() override {
+    }
+};
+
+template<typename T>
+class HeapSorter : public Sorter<T> {
+public:
+    void sort() override {
+    }
+};
+
+template<typename T>
+class StdSorter : public Sorter<T> {
+public:
+    void sort() override {
+    }
+};
+
+template<typename T>
+class SorterFactory {
+public:
+    SorterFactory(SortingSettings sorter_settings) {};
+
+private:
+    SortingSettings sorter_settings;
+
+};
 
 int main(const int argc, char *argv[]) {
     CLIParser parser(argc, argv);
